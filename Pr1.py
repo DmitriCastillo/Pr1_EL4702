8b8a00eea9db51388ba861a18743935b75f22a23
DmitriCastillo <160551322+DmitriCastillo@users.noreply.github.com>
2024-02-24 17:40:06 -0600

    Update Pr1.py

--- Pr1.py
+++ Pr1.py
@@ -1,2 +1,22 @@
-#Archivo editado
-print("pr1")
+# Importamos los módulos necesarios
+import math
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+
+# Importamos los datos del excel
+ArchivoXl =pd.read_excel(r'C:\Users\dmitr\Documents\TEC\1 Semestre 2024\Probabilidad y Estadistica\Proyecto 1\AirQualityUCI.xlsx')
+columna = ArchivoXl['C6H6(GT)']
+arreglo = np.array(columna)
+print(arreglo)
+
+# Generamos los datos para el gráfico
+x = np.array(range(10))
+y = np.zeros(len(x))
+for i in range(len(x)):
+    y[i] = math.sin(x[i])
+
+# Creamos el gráfico
+plt.ion()
+plt.plot(x,y)
+plt.xlabel("Calidad del aire")
